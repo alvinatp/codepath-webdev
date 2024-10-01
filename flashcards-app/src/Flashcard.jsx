01c079c80c4ef3bc541ffec12b3e3e3773ad5264
Alvin Phan <alvintp2@uci.edu>
2024-09-30 22:30:16 -0700

    implemented flipping animation

--- flashcards-app/src/Flashcard.jsx
+++ flashcards-app/src/Flashcard.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 
 const Flashcard = ({ question, answer, showAnswer, onClick }) => {
   return (
-    <div className="flashcard" onClick={onClick}>
-      <div>{showAnswer ? answer : question}</div>
+    <div className="flashcard-container" onClick={onClick}>
+      <div className={`flashcard ${showAnswer ? 'flipped' : ''}`}>
+        <div className="flashcard-front">
+          <div>{question}</div>
+        </div>
+        <div className="flashcard-back">
+          <div>{answer}</div>
+        </div>
+      </div>
     </div>
   );
 };
